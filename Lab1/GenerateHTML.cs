de3f17e0aa7f45a9febceb813e84bfe6f2524dcf
DESKTOP-OHVVGQ9\Patryk <patryk.piotrowski19@gmail.com>
2018-03-06 11:04:01 +0100

    Dodano lab1

---
+++ Lab1/GenerateHTML.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Lab1
+{
+    class GenerateHTML
+    {
+
+        #region Zmienne
+
+        List<string> lines = new List<string>();
+        bool _addedFooter;
+        bool _addedHeader;
+
+        #endregion
+
+
+        #region Konstruktor
+
+        public GenerateHTML()
+        {
+            _addedFooter = false;
+            _addedHeader = false;
+            AddHtmlHeader();
+        }
+
+        public GenerateHTML(string inputFile, string outputFile)
+        {
+
+            try
+            {
+                AddHtmlHeader();
+                string line;
+                StreamReader sr = new StreamReader(inputFile);
+
+                // Open the text file using a stream reader.
+                while((line = sr.ReadLine()) != null)
+                {
+                    List<string> rowElements = new List<string>();
+
+                    string []readedLine = line.Split();
+
+                    foreach (string actualLine in readedLine)
+                    {
+                        rowElements.Add(actualLine);
+                    }
+                    AddRow(rowElements);
+                }
+                SaveToFile(outputFile);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Nie można znalezc pliku");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        #endregion
+
+
+        #region Metody odpowiedzialne za wstawianie elementów strony
+
+        private void AddHtmlHeader()
+        {
+            if (!_addedHeader)
+            {
+                _addedHeader = true;
+                lines.Add("<html><head></head><body><table border='1'>");
+            }
+        }
+
+        private void AddHtmlFooter()
+        {
+            if (!_addedFooter)
+            {
+                _addedFooter = true;
+                lines.Add("</table></body></html>");
+            }
+
+        }
+
+        public void AddRow(List<string> dataElements)
+        {
+            lines.Add("<tr>");
+
+            foreach(string elements in dataElements)
+            {               
+                lines.Add("<td>"+elements+"</td>");
+            }
+                lines.Add("</tr>");
+        }
+
+        public void AddRow(List<string> dataElements, bool opt)
+        {
+            //Opt = 0 nagłowek
+            //Opt = 1 stopka
+
+            switch(opt)
+            {
+                case false:
+                    lines.Add("<thead>");
+                    AddRow(dataElements);
+                    lines.Add("</thead>");
+                    break;
+                case true:
+                    lines.Add("<tfoot>");
+                    AddRow(dataElements);
+                    lines.Add("</tfoot>");
+                    break;
+            }
+
+        }
+
+
+        #endregion
+
+
+        #region Zapis do pliku
+
+        public void SaveToFile(string filename)
+        {
+            AddHtmlFooter();
+
+            using (System.IO.StreamWriter outputFile = new System.IO.StreamWriter(filename))
+            {
+                foreach (string line in lines)
+                    outputFile.WriteLine(line);
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab1
+{
+    class Program
+    {
+
+        static void GenerateHtmlForStrings()
+        {
+            List<string> stringElements = new List<string>();
+            stringElements.Add("Zm1");
+            stringElements.Add("Zm2");
+            stringElements.Add("Zm3");
+
+            GenerateHTML _generateHTML = new GenerateHTML();
+            _generateHTML.AddRow(stringElements);
+            _generateHTML.AddRow(stringElements);
+
+
+            _generateHTML.SaveToFile("index.html");
+        }
+
+        static void GenerateHtmlForNumbers()
+        {
+            List<string> numberElements = new List<string>();
+            List<string> numberElements2 = new List<string>();
+
+            numberElements.Add(30);
+            numberElements.Add(55.ToString());
+
+            numberElements2.Add(192.ToString());
+            numberElements2.Add(42.55f.ToString());
+
+
+            GenerateHTML _generateHTML = new GenerateHTML();
+            _generateHTML.AddRow(numberElements);
+            _generateHTML.AddRow(numberElements2);
+
+
+            _generateHTML.SaveToFile("index2.html");
+        }
+
+        static void GenerateHtmlFromFile()
+        {
+            GenerateHTML _generateHTML = new GenerateHTML("inputFiles.txt", "index3.html");
+        }
+
+        static void GenerateHtmlWithHeaderAndFooter()
+        {
+            GenerateHTML _generateHTML = new GenerateHTML();
+
+            List<string> headerElements = new List<string>();
+            List<string> stringElements = new List<string>();
+            List<string> footerElements = new List<string>();
+            headerElements.Add("Kolumna1");
+            headerElements.Add("Kolumna2");
+
+            footerElements.Add("Stopka1");
+            footerElements.Add("Stopka2");
+
+            stringElements.Add("Zm1");
+            stringElements.Add("Zm2");
+
+
+            _generateHTML.AddRow(headerElements,false);
+
+            for(int i=0;i<5;i++)
+            {
+                _generateHTML.AddRow(stringElements);
+            }
+
+            _generateHTML.AddRow(footerElements, true);
+            _generateHTML.SaveToFile("index4.html");
+
+        }
+
+        static void Main(string[] args)
+        {
+            GenerateHtmlForStrings();
+            GenerateHtmlForNumbers();
+            GenerateHtmlFromFile();
+            GenerateHtmlWithHeaderAndFooter();
+        }
+    }
+}
